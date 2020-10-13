1e6f2276f40cac0c324276369936f7fa2ce9c4d9
Alvin Mantovani <alvin.ictn@gmail.com>
2020-10-13 09:38:49 +0700

    add demo

---
+++ precache-manifest.2aad0069ab103e5e7eef625af5d8b062.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0700b1bebc4786012605b9de87f40fc",
+    "url": "/Le-Movie/index.html"
+  },
+  {
+    "revision": "fbfc34822432768828e9",
+    "url": "/Le-Movie/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "dd4d4c0241b14beeec94",
+    "url": "/Le-Movie/static/css/main.993147d6.chunk.css"
+  },
+  {
+    "revision": "fbfc34822432768828e9",
+    "url": "/Le-Movie/static/js/2.c3d3faf2.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Le-Movie/static/js/2.c3d3faf2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd4d4c0241b14beeec94",
+    "url": "/Le-Movie/static/js/main.9af40fd4.chunk.js"
+  },
+  {
+    "revision": "c887f1889d6f0630f4a0",
+    "url": "/Le-Movie/static/js/runtime-main.8053b057.js"
+  },
+  {
+    "revision": "2ab4a7df29c42c40647c1161d34cede7",
+    "url": "/Le-Movie/static/media/Logo.2ab4a7df.svg"
+  },
+  {
+    "revision": "70a72a7fc317363f52dab4e708881cb9",
+    "url": "/Le-Movie/static/media/thumbnail.70a72a7f.jpg"
+  },
+  {
+    "revision": "3551c8a834434d02a8de51e3a9306f25",
+    "url": "/Le-Movie/static/media/user.3551c8a8.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8b10c6d2859ecce1edccb24ae9619137.js"
+  "/Le-Movie/precache-manifest.2aad0069ab103e5e7eef625af5d8b062.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Le-Movie/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
